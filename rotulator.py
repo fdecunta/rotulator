63dd366f225079d10ea6f33912149ffabcafa34e
fd-lenovo <fdecunta@agro.uba.ar>
2023-02-04 02:10:25 -0300

    Arregle bug de rotulos desordenados

--- rotulator.py
+++ rotulator.py
@@ -11,8 +11,7 @@ import yaml
 import argparse
 import itertools
 import random
-import labelsCreator
-
+import csv
 
 def read_yaml(filepath):
     # Read experiment info from yaml file
@@ -74,12 +73,81 @@ def create_df(exp_design, options):
     return df_lines
 
 
+def get_longest_string(csv_list):
+    """Funcion accesoria de create_labels.
+    Buscar las palabras mas largas: el factor y el nivel mas largo posible"""
+    longest_factor_name = 0
+    longest_lvl_name = 0
+    for i in csv_list:
+        for key,value in i.items():
+            if len(key) > longest_factor_name:
+                longest_factor_name = len(key)
+            if len(value) > longest_lvl_name:
+                longest_lvl_name = len(value)
 
-def create_rotulos(csv_file):
-    exp_name = os.path.basename(csv_file)
-    dest_name = labelsCreator.create_labels(csv_file, exp_name)
-    print(f'Se creó {dest_name}')
+    return longest_factor_name, longest_lvl_name
 
+
+def create_labels(csv_file):
+    A4_COLS = 80                # Un word con letra tamaño 12 banca 80 caracteres. Habria que sacar cuentas con espacios y demas
+
+    rotulos = []
+    with open(csv_file, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rotulos = [row for row in reader]
+
+    separador = "\t"
+    
+    # Get longest names to format the output
+    longest_factor_name, longest_lvl_name = get_longest_string(rotulos)
+    longest_row = longest_lvl_name + longest_factor_name + 2 # 2 espacios: uno entre factor/nivel y otro entre rotulos contiguos
+
+    if longest_row < PR_COLS:
+        print_columns = 3
+    else:
+        print_columns = 2
+
+    # 
+    # TODO!!! Chequear si entran en 3 columnas o si hay que hacerlo en 2
+    # 
+
+    # Iterar sobre cada fila y armar sus rotulos
+    rotulos_list = []
+    printed_rows = 0
+    rows_per_rotulo = len(rotulos[0].keys()) # Keys porque es un diccionario
+
+    while len(rotulos) > 0:
+        # Obtengo lista de 3 rotulos
+        for i in range(0,print_columns):
+            if len(rotulos) != 0:
+                rotulos_list.append(rotulos.pop(0))
+
+        # Armo lista de strings vacios: uno por cada fila de rotulo
+        strlist = ["" for i in range(0,rows_per_rotulo)]
+        
+        # Para los 3 rotulos armo el string de cada linea
+        col_n = 0
+        for i in range(0,3):
+            rotulo = rotulos_list.pop(0)
+
+            line_number = 0
+            if col_n == 0:      # Si es la primera columna de rotulos no tiene que dejar espacio
+                separador = ""
+            else:
+                separador = "  "
+
+            for key,value in rotulo.items():
+                rotulo_str = f'{key:<{longest_factor_name}},{value:>{longest_lvl_name}},'
+                # rotulo_str = f'{separador}{key},{value},'
+                # strlist[line_number] = strlist[line_number] + rotulo_str
+                strlist[line_number] = strlist[line_number] + separador + rotulo_str
+                line_number += 1
+            col_n += 1
+
+        for i in strlist:
+            sys.stdout.write(i + "\n")
+            printed_rows += 1
+        sys.stdout.write("\n")
 
 
 def main():
@@ -88,14 +156,21 @@ def main():
                                      epilog = 'fd - 2023')
     parser.add_argument('-r', action='store_true', default=False,
                         help='Random: ordena los IDs al azar')
-    parser.add_argument('yaml_input')
+    parser.add_argument('-l', action='store_true', default=False,
+                        help="Labels: arma rotulos para macetas a partir de un archivo CSV")
+    parser.add_argument('input_file')
     options = parser.parse_args()    
 
-    yaml_file = options.yaml_input
+    if options.l:
+        create_labels(options.input_file)
+        exit(0)
+        
+
+    yaml_file = options.input_file
     exp_design = read_yaml(yaml_file)
     df_lines = create_df(exp_design, options)
-
     sys.stdout.writelines(df_lines)
+    exit(0)
 
 
 if __name__ == "__main__":
